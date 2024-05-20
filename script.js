af67ab98d2c38df9590d0e2a49d02b677df39b65
Prakash Mondal <97402824+rko0211@users.noreply.github.com>
2024-05-20 18:30:17 +0530

    Add initial commit

---
+++ script.js
@@ -0,0 +1,54 @@
+let search_item = document.querySelector(".search-item");
+let search_button = document.querySelector(".search-button");
+let get_result = document.querySelector(".get-result");
+search_button.addEventListener('click', () => {
+  const country_name = search_item.value;
+  if (country_name === "") {
+    alert("PLease Enter Some valid Country Name");
+    return;
+  }
+  const final_url = `https://restcountries.com/v3.1/name/${country_name}?fullText=true`;
+  fetch(final_url).then((Response) => Response.json()).then((data) => {
+    // console.log(data);
+
+    get_result.innerHTML = `<div class="country_section">
+<img src= "${data[0].flags.png} " alt="Country Flag" class="country-image">
+<h2 class="country-name">${data[0].name.common}</h2>
+</div>
+
+<div class="county-info">
+<div class="entry1">
+  <b>Capital: </b>
+  <span class="cap_name common">${data[0].capital[0]}</span>
+</div>
+<div class="entry2">
+  <b>Contient: </b>
+  <span class="cont_name common">${data[0].continents[0]}</span>
+</div>
+<div class="entry3">
+  <b>Population: </b>
+  <span class="Population common">${data[0].population}</span>
+</div>
+
+<div class="entry4">
+  <b>Currency: </b>
+  <span class="currency common">${data[0].currencies[Object.keys(data[0].currencies)].name} - ${Object.keys(data[0].currencies)[0]}</span>
+</div>
+
+<div class="entry5">
+  <b>Common Languages: </b>
+  <span class="language common">${Object.values(data[0].languages).toString().split(",").join(", ")}</span>
+</div>
+
+<div class="entry6">
+  <a href="${data[0].maps.googleMaps}" target="_blank">See Location</a>
+</div>
+<div class="entry6 street">
+  <a href="${data[0].maps.openStreetMaps}" target="_blank">Street Map</a>
+</div>
+</div>`;
+  })
+    .catch(() => {
+      get_result.innerHTML = `<h3 class ="error">Please Enter Valid Country Name.</h3>`
+    })
+});
